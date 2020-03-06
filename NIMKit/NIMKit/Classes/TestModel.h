ad5a651beec5047ac7fa915509f57ea8eca4b2ed
李磊涛 <lileitao@gaosiedu.com>
2020-03-06 16:37:54 +0800

    测试添加TestModel

---
+++ NIMKit/NIMKit/Classes/TestModel.h
@@ -0,0 +1,16 @@
+//
+//  TestModel.h
+//  NIMKit
+//
+//  Created by 李磊涛 on 2020/3/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModel : RootView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NIMKit/NIMKit/Classes/TestModel.m
@@ -0,0 +1,12 @@
+//
+//  TestModel.m
+//  NIMKit
+//
+//  Created by 李磊涛 on 2020/3/6.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
